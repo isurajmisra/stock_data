b01ce1d262fa3fd59f4e234b8a3d0b7828b2171c
Suraj Mishra <isurajmisra@gmail.com>
2020-12-18 03:26:02 -0500

    home url added

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_data/', views.get_stock_data, name="get_data"),
+    path('', views.get_stock_data, name="get_data"),
     path('api/get_intraday_data/', views.get_intraday_data, name="get_intraday_data"),
 
 ]
