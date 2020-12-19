19125b318d39d365b280b7be7d30c628f4c9c02f
Suraj Mishra <57786599+isurajmisra@users.noreply.github.com>
2020-12-20 02:39:36 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -90,22 +90,21 @@ def home(request):
 def get_option_data(symbol):
     new_url = f'https://www.nseindia.com/api/option-chain-indices?symbol={symbol}'
     referer = f"https://www.nseindia.com/get-quotes/derivatives?symbol={symbol}"
-    cookie_dict = {
-        "bm_sv": "F140DF14529DCBD46F41C0CA2BF24EE7~kbGVwkURJC0sZAK/dfkMMNdp+I5+fHncaM2faFXjIOis8SUGWPA/GL7or2MbB6YbioUNsFqm3nrGbRNslXEU7WSilb4gIqms3So5NhURNXUP9myu2XvCRZ3tTZ+zWzH5ID/z7AVtakwD4BwJlLTxGmikyK3IYlREFTfHDs+N42c="}
+    cookie_dict = dict(bm_sv= "F140DF14529DCBD46F41C0CA2BF24EE7~kbGVwkURJC0sZAK/dfkMMNdp+I5+fHncaM2faFXjIOis8SUGWPA/GL7or2MbB6YbioUNsFqm3nrGbRNslXEU7WSilb4gIqms3So5NhURNXUP9myu2XvCRZ3tTZ+zWzH5ID/z7AVtakwD4BwJlLTxGmikyK3IYlREFTfHDs+N42c=")
     headers = {
                'user-agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0",
                "accept-language": "en-US,en;q=0.9", "accept-encoding": "gzip,deflate,br"
                }
 
-    s = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    s.mount('http://', adapter)
-    s.mount('https://', adapter)
-    for cookie in cookie_dict:
-        s.cookies.set(cookie, cookie_dict[cookie])
+    with requests.Session() as s:
+    #retry = Retry(connect=3, backoff_factor=0.5)
+    #adapter = HTTPAdapter(max_retries=retry)
+    #s.mount('http://', adapter)
+    #s.mount('https://', adapter)
+    #for cookie in cookie_dict:
+        #s.cookies.set(cookie, cookie_dict[cookie])
 
-    page = requests.get(new_url, headers=headers)
+        page = s.get(new_url, headers=headers,cookies=cookie_dict)
 
     return page
 
