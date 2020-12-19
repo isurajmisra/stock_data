4937b628c307d62f839afa09a48ba67d41048104
Suraj Mishra <57786599+isurajmisra@users.noreply.github.com>
2020-12-20 02:01:52 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -91,7 +91,7 @@ def get_option_data(symbol):
     referer = f"https://www.nseindia.com/get-quotes/derivatives?symbol={symbol}"
     cookie_dict = {
         "bm_sv": "F140DF14529DCBD46F41C0CA2BF24EE7~kbGVwkURJC0sZAK/dfkMMNdp+I5+fHncaM2faFXjIOis8SUGWPA/GL7or2MbB6YbioUNsFqm3nrGbRNslXEU7WSilb4gIqms3So5NhURNXUP9myu2XvCRZ3tTZ+zWzH5ID/z7AVtakwD4BwJlLTxGmikyK3IYlREFTfHDs+N42c="}
-    headers = {"referer": referer,
+    headers = {
                'user-agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0",
                "accept-language": "en-US,en;q=0.9", "accept-encoding": "gzip,deflate,br"
                }
