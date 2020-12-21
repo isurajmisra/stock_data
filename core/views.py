176a2e58100af3cd0facf6f010f19661a37060b2
Suraj Mishra <isurajmisra@gmail.com>
2020-12-21 16:35:58 +0530

    modified api

--- core/views.py
+++ core/views.py
@@ -39,7 +39,7 @@ def api_get_data(request):
 
     symbol = request.GET.get('symbol') or "BANKNIFTY"
     print(symbol)
-
+    today = datetime.date.today()
     try:
         ti = datetime.datetime.now().strftime("%H:%M")
         if  ti> "09:30" and ti < "16:30":
@@ -50,58 +50,69 @@ def api_get_data(request):
         print(e)
         context = {}
         return JsonResponse(context, safe=False)
+    try:
+        dajs = json.loads(page.text)
+        expiry_dt = dajs['records']['expiryDates'][0]
 
-    dajs = json.loads(page.text)
-    expiry_dt = dajs['records']['expiryDates'][0]
+        ce_values = [data['CE'] for data in dajs['records']['data'] if "CE" in data and data['expiryDate'] == expiry_dt]
 
-    ce_values = [data['CE'] for data in dajs['records']['data'] if "CE" in data and data['expiryDate'] == expiry_dt]
+        pe_values = [data['PE'] for data in dajs['records']['data'] if "PE" in data and data['expiryDate'] == expiry_dt]
 
-    pe_values = [data['PE'] for data in dajs['records']['data'] if "PE" in data and data['expiryDate'] == expiry_dt]
-
-    ce_df = pd.DataFrame(ce_values, columns=['changeinOpenInterest', 'strikePrice'])
-    pe_df = pd.DataFrame(pe_values, columns=['changeinOpenInterest', 'strikePrice'])
+        ce_df = pd.DataFrame(ce_values, columns=['changeinOpenInterest', 'strikePrice'])
+        pe_df = pd.DataFrame(pe_values, columns=['changeinOpenInterest', 'strikePrice'])
 
 
-    ce_df['strikePrice'] = ce_df['strikePrice'].astype(int)
-    index_quote_lastPrice = int(dajs['records']['underlyingValue'])
-    ce_df_sort = ce_df.iloc[(ce_df['strikePrice'] - index_quote_lastPrice).abs().argsort()[:1]].index.tolist()
-    pe_df_sort = pe_df.iloc[(pe_df['strikePrice'] - index_quote_lastPrice).abs().argsort()[:1]].index.tolist()
-    cr1 = ce_df_sort[0] - 7
-    cr2 = ce_df_sort[0] + 8
-    pr1 = pe_df_sort[0] - 7
-    pr2 = pe_df_sort[0] + 8
-    ce_dt = ce_df[cr1:cr2]
-    pe_dt = pe_df[pr1:pr2]
+        ce_df['strikePrice'] = ce_df['strikePrice'].astype(int)
+        index_quote_lastPrice = int(dajs['records']['underlyingValue'])
+        ce_df_sort = ce_df.iloc[(ce_df['strikePrice'] - index_quote_lastPrice).abs().argsort()[:1]].index.tolist()
+        pe_df_sort = pe_df.iloc[(pe_df['strikePrice'] - index_quote_lastPrice).abs().argsort()[:1]].index.tolist()
+        cr1 = ce_df_sort[0] - 7
+        cr2 = ce_df_sort[0] + 8
+        pr1 = pe_df_sort[0] - 7
+        pr2 = pe_df_sort[0] + 8
+        ce_dt = ce_df[cr1:cr2]
+        pe_dt = pe_df[pr1:pr2]
 
-    ce_dt['changeinOpenInterest'] = ce_dt['changeinOpenInterest'].astype(int) * 25
-    pe_dt['changeinOpenInterest'] = pe_dt['changeinOpenInterest'].astype(int) * 25
-    ce_dt['strikePrice'] = ce_dt['strikePrice'].astype(int)
-    pe_dt['strikePrice'] = pe_dt['strikePrice'].astype(int)
-    ce_changeInOpenInterest_sum = ce_dt['changeinOpenInterest'].sum()
-    pe_changeInOpenInterest_sum = pe_dt['changeinOpenInterest'].sum()
-    diff_changeinOpenInterest = pe_changeInOpenInterest_sum - ce_changeInOpenInterest_sum
+        ce_dt['changeinOpenInterest'] = ce_dt['changeinOpenInterest'].astype(int) * 25
+        pe_dt['changeinOpenInterest'] = pe_dt['changeinOpenInterest'].astype(int) * 25
+        ce_dt['strikePrice'] = ce_dt['strikePrice'].astype(int)
+        pe_dt['strikePrice'] = pe_dt['strikePrice'].astype(int)
+        ce_changeInOpenInterest_sum = ce_dt['changeinOpenInterest'].sum()
+        pe_changeInOpenInterest_sum = pe_dt['changeinOpenInterest'].sum()
+        diff_changeinOpenInterest = pe_changeInOpenInterest_sum - ce_changeInOpenInterest_sum
 
-    if diff_changeinOpenInterest > 1000000:
-        call = "Buy Call Option"
-    elif diff_changeinOpenInterest < -1000000:
-        call = "Buy Put Option"
-    else:
-        call = "Neutral"
+        if diff_changeinOpenInterest > 1000000:
+            call = "Buy Call Option"
+        elif diff_changeinOpenInterest < -1000000:
+            call = "Buy Put Option"
+        else:
+            call = "Neutral"
 
-    print(dajs['records']['timestamp'])
-    date_time_obj = datetime.datetime.strptime(dajs['records']['timestamp'], '%d-%b-%Y %H:%M:%S')
-    ce_dt['timestamp'] = date_time_obj
-    pe_dt['timestamp'] = date_time_obj
-    today = datetime.date.today()
-    result = []
-    intraday_data = IntradayData.objects.filter(symbol=symbol, time__date=today)
-    for data in intraday_data:
-        result.append({"symbol": data.symbol, "signal": data.signal,
-               "diff": str(data.diff),
-               "call": str(data.call),
-               "put": str(data.put),
-               "time": data.time
-               })
+        print(dajs['records']['timestamp'])
+        date_time_obj = datetime.datetime.strptime(dajs['records']['timestamp'], '%d-%b-%Y %H:%M:%S')
+        ce_dt['timestamp'] = date_time_obj
+        pe_dt['timestamp'] = date_time_obj
+        dat_time_date = date_time_obj.date()
+
+        if today == dat_time_date:
+            if IntradayData.objects.filter(time=date_time_obj).first() is None:
+                intraday_data = IntradayData.objects.create(symbol=symbol, call=ce_changeInOpenInterest_sum,
+                                                            put=pe_changeInOpenInterest_sum,
+                                                            diff=diff_changeinOpenInterest, time=date_time_obj,
+                                                            signal=call)
+                intraday_data.save()
+    except Exception:
+        print(Exception)
+
+        result = []
+        intraday_data = IntradayData.objects.filter(symbol=symbol, time__date=today)
+        for data in intraday_data:
+            result.append({"symbol": data.symbol, "signal": data.signal,
+                   "diff": str(data.diff),
+                   "call": str(data.call),
+                   "put": str(data.put),
+                   "time": data.time
+                   })
     # context = {"symbol": symbol, "call": call,
     #            "diff_changeinOpenInterest": str(diff_changeinOpenInterest),
     #            "ce_sum": str(ce_changeInOpenInterest_sum),
@@ -110,14 +121,7 @@ def api_get_data(request):
     #            "ce_data":ce_dt.to_html(classes='table table-striped'),
     #            "pe_data":pe_dt.to_html(classes='table table-striped')
     #            }
-    dat_time_date = date_time_obj.date()
 
-    if today == dat_time_date:
-        if IntradayData.objects.filter(time=date_time_obj).first() is None:
-
-            intraday_data = IntradayData.objects.create(symbol=symbol,call=ce_changeInOpenInterest_sum, put=pe_changeInOpenInterest_sum,
-                                                    diff=diff_changeinOpenInterest, time=date_time_obj, signal=call)
-            intraday_data.save()
 
     return JsonResponse(result, safe=False)
 
