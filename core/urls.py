a17e3468f470737f99a46b0e6221259e5a6a3f3d
Suraj Mishra <57786599+isurajmisra@users.noreply.github.com>
2023-05-04 01:37:20 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('option_chart/', views.get_chart, name="option_chart"),
     path('api/get_intraday_data/', views.get_intraday_data, name="get_intraday_data"),
     path('api/api_get_data/', views.api_get_data, name="api_get_data"),
-    path('api/api_get_data/', views.api_get_data, name="api_get_data"),
 
 ]
 
