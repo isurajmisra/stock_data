612f62cb767c2e497fff3f5a979fce1797175463
Suraj Mishra <57786599+isurajmisra@users.noreply.github.com>
2023-05-04 02:56:20 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('option_chart/', views.get_chart, name="option_chart"),
     path('api/get_intraday_data/', views.get_intraday_data, name="get_intraday_data"),
     path('api/api_get_data/', views.api_get_data, name="api_get_data"),
